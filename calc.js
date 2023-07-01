8a53387456064c156a8b703cdb887ae9cff21153
alangon03 <alangon03@gmail.com>
2023-07-01 15:45:13 -0700

    Create basic math functions

---
+++ calc.js
@@ -0,0 +1,12 @@
+// Create varibles for math
+let a = null;
+let b = null;
+let operator;
+
+// Create math functions
+const add = (a,b) => a+b;
+const multiply = (a,b) => a*b;
+const subtract = (a,b) => a-b;
+const divide = (a,b) => a/b;
+const percent = (a) => a/100;
+const invert = (a) => a*-1;
